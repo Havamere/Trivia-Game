4c124ca0e478ccfe393d9cec5c812dceff878cd3
Kyle Rinehart <Kyle Rinehart>
2016-05-03 21:24:01 -0400

    Css additions made to theme website in an Earthy style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,25 @@
 //object block question and answer
 var trivia = [
-	{number: 0, question: "Where is the highest point in the world?", answer: "Mount Chimborazo"},
+	{number: 0, question: "What mountain would you go to, to be closest to space?", answer: "Mount Chimborazo"},
 	{number: 1, question: "In what country can you weigh less, just by living there?", answer: "Canada"},
-	{number: 2, question: "Hurg de gurge in flibben flabben?", answer: "Foobar"},
-	{number: 3, question: "How are you feeling?", answer: "Great"}
+	{number: 2, question: "How fast are you spinning around the Earth's core when you are standing on the equator?", answer: "1000 MPH"},
+	{number: 3, question: "How long would a day on Earth be without the moon??", answer: "6-8 HRS"},
+	{number: 4, question: "Which volcano is so active, it is constantly erupting and has been nicknamed 'The Lighthouse of the Mediterranean?", answer: "Stromboli Volcano"},
+	{},
+	{},
+	{},
 ]
 
 //object block stores other answers
 var notAnswers = [
 	["Mount Kilimanjaro", "Mount Everest", "Mount McKinley"],
 	["Europe", "Chile", "Japan"],
-	["Bass", "Heliotrope", "Teppanyaki"],
-	["Tired", "Bad", "Malcontent"]
+	["300 MPH", "500 MPH", "2000 MPH"],
+	["48-50 HRS", "12 HRS", "2-4 HRS"],
+	["Mount Etna", "Santa Margarida Volcano", "Ischia Island"],
+	[],
+	[],
+	[],
 ]
 
 //randomizer for answer placement
@@ -31,9 +39,7 @@ function question(array1,array2) {
 		array2[i].push(array1[i].answer);
 		$('#questionaire').append("<p id="+"Q"+i+">"+array1[i].question+"</p>");
 		array2[i] = possAnswers(array2[i]);
-		console.log(array2[i]);
 		for(var j=0; j<array2[i].length; j++){
-			console.log(array2[i][j]);
 		 	$('#questionaire').append("<input type="+"radio"+" name="+"question"+i+" value="+"\""+array2[i][j]+"\""+" id="+"question"+i+">"+array2[i][j]+"</input>");
 		}
 	}
@@ -55,45 +61,40 @@ function timeConverter (t) {
     return minutes + ":" + seconds;
  }
 //timer for questionaire
-$(document).ready(function(){
-	setTimeout(function(){
-		alert("You are about to test your knowledge of some of Planet Earth's strange facts.");
-		alert("Once you clear this message, you will have 5 minutes to answer 10 questions.  Good luck!")
-	},1000);
-	time = 10
-	$("#timer").html("05:00");
-	function countDown(){
-		time--;
-		currentTime = timeConverter(time);
-		$("#timer").html(currentTime);
-		console.log(time);
-		//Time's Up mechanic
-		if (time == 0) {
-			for (var e=0; e<trivia.length; e++){
-				if ($('#question'+e+':checked').val() == trivia[e].answer){
-					correctAnswers++;
-				} else if ($('#question'+e+':checked').val() == undefined){
-					notAnswered++;
-				} else {
-					incorrectAnswers++;
-				}
-			}
-			$('#main-section').empty();
-			$('#main-section').append("<h1>Time\'s Up!</h1>");
-			$('#main-section').append("<p>You got "+correctAnswers+" correct answers!</p>");
-			$('#main-section').append("<p>You got "+incorrectAnswers+" incorrect answers.</p>");
-			$('#main-section').append("<p>You got "+notAnswered+" unanswered answers.</p>");
-		}
-	} 
-	counter = setInterval(countDown,1000);
-})
+// $(document).ready(function(){
+// 	setTimeout(function(){
+// 		alert("You are about to test your knowledge of some of Planet Earth's strange facts.");
+// 		alert("Once you clear this message, you will have 5 minutes to answer 10 questions.  Good luck!")
+// 	},1000);
+// 	time = 10
+// 	$("#timer").html("05:00");
+// 	function countDown(){
+// 		time--;
+// 		currentTime = timeConverter(time);
+// 		$("#timer").html(currentTime);
+// 		//Time's Up mechanic
+// 		if (time == 0) {
+//			// Checks user answers versus
+// 			for (var e=0; e<trivia.length; e++){
+// 				if ($('#question'+e+':checked').val() == trivia[e].answer){
+// 					correctAnswers++;
+// 				} else if ($('#question'+e+':checked').val() == undefined){
+// 					notAnswered++;
+// 				} else {
+// 					incorrectAnswers++;
+// 				}
+// 			}
+// 			$('#main-section').empty();
+// 			$('#main-section').append("<h1>Time\'s Up!</h1>");
+// 			$('#main-section').append("<p>You got "+correctAnswers+" correct answers!</p>");
+// 			$('#main-section').append("<p>You got "+incorrectAnswers+" incorrect answers.</p>");
+// 			$('#main-section').append("<p>You got "+notAnswered+" unanswered answers.</p>");
+// 		}
+// 	} 
+// 	counter = setInterval(countDown,1000);
+// })
 
 //stores player selections into array
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var notAnswered = 0;
-
-//checks player answers against correct answers
-	// for (var a=0; a<trivia.length; a++) {
-	// 	if ($("name="+"question"+a)){}
-	// }
